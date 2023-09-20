4b64e5bfeebd463127cd35f81b5d9157b64b0260
NyashaV <ny_vanessa@hotmail.co.uk>
2023-09-20 19:15:39 +0100

    Installed axios, intergrated API and made minor CSS adjustments

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         {" "}
-        <div className="contrastContainer">
+        <div className="contrastContainer" style={{ width: "50rem" }}>
           <header className="App-header">
             <img src={logo} className="App-logo img-fluid" alt="logo" />
             <h1 className="text-center appName">The Pink Dictionary </h1>
@@ -20,6 +20,7 @@ export default function App() {
           <footer className="text-center mt-5">
             Coded by{" "}
             <a
+              rel="noreferrer"
               target="_blank"
               href="https://uk.linkedin.com/in/nyasha-tavatya-534581204?challengeId=AQFMzbsFVlvN7AAAAYqqAKP3pzKay_2chmRrHnq1NeqJLlT8kIewtEI4k60mMAuumrameH9TxAJh7W3_ttPqDOyJ4NKyGH8uog&submissionId=eef22730-8818-8617-f995-f69c30444701&challengeSource=AgEQ-0ZEpbRkzAAAAYqqAOqnDEr4Wr6sChiP-cHOeKNRtd_o6rEB4VRCQE8EE6I&challegeType=AgEgoTUluUzbLQAAAYqqAOqq9Claav6ahVaWwIBaZbt7TLl1J_9wfHs&memberId=AgFG0E2JyN6SIQAAAYqqAOqunUHN9vP7qEBvSLASXjMZYzQ&recognizeDevice=AgHRcuSmekVUMQAAAYqqAOqxC6TX4JwTlX3nHqxAkXYflArwlRAp"
             >
